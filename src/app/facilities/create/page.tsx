755ad7169de816228dd3d4628e15b6e3c728b3f3
Crispas Makanani <crispasmakanani@gmail.com>
2024-05-27 13:30:06 +0200

    final ccommit

--- src/app/facilities/create/page.tsx
+++ src/app/facilities/create/page.tsx
@@ -1,55 +1,51 @@
-// app/facilities/create/page.tsx
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import FacilityForm from "../../components/FacilityForm";
-import { Typography } from "@mui/material";
+// // app/facilities/create/page.tsx
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import FacilityForm from "../../components/FacilityForm";
+// import { Typography } from "@mui/material";
 
-interface District {
-  id: string;
-  name: string;
-}
+// interface District {
+//   id: string;
+//   name: string;
+// }
 
-interface Owner {
-  id: string;
-  name: string;
-}
+// interface Owner {
+//   id: string;
+//   name: string;
+// }
 
-const CreateFacility: React.FC = () => {
-  const [districts, setDistricts] = useState<District[]>([]);
-  const [owners, setOwners] = useState<Owner[]>([]);
+// const CreateFacility: React.FC = () => {
+//   const [districts, setDistricts] = useState<District[]>([]);
+//   const [owners, setOwners] = useState<Owner[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const districtsResponse = await axios.get(
-        "https://zipatala.health.gov.mw/api/districts"
-      );
-      const ownersResponse = await axios.get(
-        "https://zipatala.health.gov.mw/api/owners"
-      );
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const districtsResponse = await axios.get(
+//         "https://zipatala.health.gov.mw/api/districts"
+//       );
+//       const ownersResponse = await axios.get(
+//         "https://zipatala.health.gov.mw/api/owners"
+//       );
 
-      setDistricts(districtsResponse.data);
-      setOwners(ownersResponse.data);
-    };
+//       setDistricts(districtsResponse.data);
+//       setOwners(ownersResponse.data);
+//     };
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  const handleFormSubmit = (newFacility: any) => {
-    // Handle the form submission, e.g., update state or redirect to the home page
-  };
+//   return (
+//     <>
+//       <Typography variant="h4" component="h1" gutterBottom>
+//         Create New Facility
+//       </Typography>
+//       <FacilityForm
+//         districts={districts}
+//         owners={owners}
+//         onFormSubmit={handleFormSubmit}
+//       />
+//     </>
+//   );
+// };
 
-  return (
-    <>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Create New Facility
-      </Typography>
-      <FacilityForm
-        districts={districts}
-        owners={owners}
-        onFormSubmit={handleFormSubmit}
-      />
-    </>
-  );
-};
-
-export default CreateFacility;
+// export default CreateFacility;
